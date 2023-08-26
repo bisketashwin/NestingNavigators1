08431456fff7a2a0003f26f7ec45be2ecdd99304
bisketashwin <bisketashwin@gmail.com>
2023-08-26 16:46:12 +0530

    WIP-wirking with tabs

--- App.tsx
+++ App.tsx
@@ -1,14 +1,44 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Settings, StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Feed from './components/Feed';
+import Messages from './components/Messages';
+import Profile from './components/Profile';
+import AppSettings from './components/AppSettings';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+const Tab = createBottomTabNavigator()
+function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Messages" component={Messages} />
+    </Tab.Navigator>
   );
 }
+
+const Stack = createNativeStackNavigator();
+
+const App =() =>{
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="AppSettings" component={AppSettings} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +48,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+
+
